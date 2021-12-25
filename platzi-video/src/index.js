a366a3c54c3965795bccc9dc425ca1142b7b0d26
miluska <miluskapajuelo@gmail.com>
2021-12-25 08:46:06 -0500

    add devtools configuration

--- platzi-video/src/index.js
+++ platzi-video/src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import reducer from './reducers'
 import App from './App';
 import initialState from './utils/data'
+
 /* const initialState = useInitialState("https://raw.githubusercontent.com/xsrpm/frontend-nodejs/master/platzi-video/initialState.json")
  *//* 
  */
-const store = createStore(reducer, initialState)
+const composeEnhansers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, initialState, composeEnhansers())
 
 ReactDOM.render(
   <Provider store={store}>
